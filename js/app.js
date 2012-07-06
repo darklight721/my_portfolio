25612902c3c448ba36892b72f37b461c9cbfe222
Roy Evan Sia <roysia2004@gmail.com>
2012-07-06 19:03:22 +0800

    added tabs.

--- js/app.js
+++ js/app.js
@@ -1,47 +1,61 @@
 (function($){
-	var _projects;
+	var _portfolio;
 	
-	var prepareMainPanel = function() {
-		if (!_projects) return;
+	var prepareGUI = function() {
+		if (!_portfolio) return;
 		
 		var mainPanel = $("#main-panel");
 		var imagePanel = $("#image-panel");
 		
 		// set size of main panel
-		mainPanel.width(_projects.props.width);
-		mainPanel.height(_projects.props.height);
+		mainPanel.width(_portfolio.props.width);
+		mainPanel.height(_portfolio.props.height);
 
-		// reposition tab label
-		$("nav").width(mainPanel.outerWidth());
+		// resize container
+		$("#container").width(mainPanel.outerWidth());
 		
 		// set size of image panel
-		imagePanel.width(_projects.props.width*_projects.projects.length);
-		imagePanel.height(_projects.props.height);
+		imagePanel.width(_portfolio.props.width*_portfolio.projects.length);
+		imagePanel.height(_portfolio.props.height);
 		
 		// setup panels and image panel
-		$.each(_projects.projects,function(index){
+		$.each(_portfolio.projects,function(index){
 			mainPanel.append("<div class='panel' data-scroll='"+ -index +"' data-link='"+ this.link +"' ></div>");
-			imagePanel.append("<div class='image'><img src='"+ this.image +"' width='"+ _projects.props.width +"' height='"+ _projects.props.height +"' alt='"+ this.name +"' /><div class='label'><span class='title'>"+ this.name +"</span>"+ this.desc +"</div></div>");
+			imagePanel.append("<div class='image'><img src='"+ this.image +"' width='"+ _portfolio.props.width +"' height='"+ _portfolio.props.height +"' alt='"+ this.name +"' /><div class='label'><span class='title'>"+ this.name +"</span>"+ this.desc +"</div></div>");
 		});
 		
 		// set size of panels
-		$(".panel").width(_projects.props.width/_projects.projects.length);
-		$(".panel").height(_projects.props.height);
+		$(".panel").width(_portfolio.props.width/_portfolio.projects.length);
+		$(".panel").height(_portfolio.props.height);
 		
 		// add event listeners to panels
 		$(".panel").hover(function(){
-			imagePanel.css("left",_projects.props.width*$(this).data("scroll")+"px");
+			imagePanel.css("left",_portfolio.props.width*$(this).data("scroll")+"px");
 		}).click(function(){
 			window.location.href = $(this).data("link");
 		});
 		
+		$(".tab").click(function(){
+			if ($(this).hasClass("selected")) return;
+			
+			$(".tab").toggleClass("selected");
+			
+			var tab = $(this).text();
+			if (tab === "Bio")
+			{
+				imagePanel.css("left",-_portfolio.props.width*_portfolio.projects.length+"px");
+			}
+			else if (tab === "Portfolio")
+			{
+				imagePanel.css("left","0px");
+			}
+			$(".panel").toggle(500);
+		});
 	};
 	
 	$.getJSON("projects.json", function(data){
-		_projects = data;
-		prepareMainPanel();
+		_portfolio = data;
+		prepareGUI();
 	});
-	
-	
 	
 })(jQuery);
